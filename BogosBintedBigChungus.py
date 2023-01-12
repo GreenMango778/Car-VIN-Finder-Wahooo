8876072aaf8a4aabb6dbb6414585f4a566ff8056
GreenMango778 <122332514+GreenMango778@users.noreply.github.com>
2023-01-12 13:16:23 +1300

    Update BogosBintedBigChungus.py

--- BogosBintedBigChungus.py
+++ BogosBintedBigChungus.py
@@ -1,6 +1,6 @@
 # The program should take a licence plate number eg. ABC123, add it to url of https://thatcar.nz/c/, and then search the car page on the website 
 # e.g. https://thatcar.nz/c/ABC123. It's doing this succesfully, but needs to specifically pull the VIN code, "7A8CJ0P0797205527" in this example. 
-# Not sure where I'm going wrong here? Ive tried xpath too and had many problems lol.
+# Not sure where I'm going wrong here? Ive tried xpath too and had many problems lol. 10/01/2023
 
 import requests
 from bs4 import BeautifulSoup
